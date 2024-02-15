65ca64f065b9002a0bb8ca9e3de5a7a9df450e6b
SORGHO Charles <atepir0@gmail.com>
2024-02-15 07:31:43 +0100

    👽feature: extraction of multiple scopes featuring multiple boundaries and unions

--- out.c
+++ out.c
@@ -1,8 +1,24 @@
 unsigned pc;
+unsigned upperBound = i_Ehrhart(P1);
+unsigned first_iteration = 1;
+#pragma omp for private(i,j) firstprivate(first_iteration) schedule(static)
+for (pc = 1; pc <= upperBound; pc++)
+{
+	if (first_iteration)
+	{
+		first_iteration = 0;
+	}
+	
+	for (i=0;i<=29;i++) {
+	  for (j=2;j<=42;j++) {
+	    S1(i,j);
+	  }
+	}
+unsigned pc;
 unsigned upperBound = i_Ehrhart(l);
 unsigned first_iteration = 1;
 #pragma omp for private(i,j,k) firstprivate(first_iteration) schedule(static)
-for (pc = 1; pc < upperBound; pc++)
+for (pc = 1; pc <= upperBound; pc++)
 {
 	if (first_iteration)
 	{
@@ -11,11 +27,13 @@ for (pc = 1; pc < upperBound; pc++)
 	
 	for (i=0;i<=29;i++) {
 	  for (j=i;j<=42;j++) {
-	    for (k=-98;k<=-24;k++) {
-	      S1(i,j,-k);
+	    if (l >= 10) {
+	      for (k=-98;;k++) {
+	        S1(i,j,-k);
+	      }
 	    }
-	    if (l <= 2) {
-	      for (k=-23;;k++) {
+	    if (l <= 9) {
+	      for (k=-98;k<=-24;k++) {
 	        S1(i,j,-k);
 	      }
 	    }
--- src/codegen.c
+++ src/codegen.c
@@ -42,46 +42,10 @@ char *write_init_section(TCD_Boundary boundary)
     return outputString;
 }
 
-void generateCode(TCD_BoundaryList boundaryList)
+void generateCodeSegment(struct clast_stmt *root, CloogOptions *options, TCD_Boundary boundary, FILE *outputFile)
 {
-    osl_scop_p scop = tcdFlowData->scop;
-    osl_statement_p statement;
-
-    CloogState *state;
-    CloogInput *input;
-    CloogOptions *options;
-    struct clast_stmt *root;
-
-    state = cloog_isl_state_malloc(isl_ctx_alloc());
-    options = cloog_options_malloc(state);
-
-    input = cloog_input_from_osl_scop(state, scop);
-
-    if (input == NULL)
-    {
-        fprintf(stderr, "Error: Unable to generate input from scop\n");
-        exit(EXIT_FAILURE);
-    }
-
-    cloog_input_dump_cloog(stdout, input, options);
-
-    root = cloog_clast_create_from_input(input, options);
-
-    if (root == NULL)
-    {
-        fprintf(stderr, "Error: Unable to generate clast from input\n");
-        exit(EXIT_FAILURE);
-    }
-    FILE *outputFile = fopen(tcdFlowData->outputFile, "w+");
-
-    if (outputFile == NULL)
-    {
-        fprintf(stderr, "Error: Unable to open file %s\n", tcdFlowData->outputFile);
-        exit(EXIT_FAILURE);
-    }
-
     // Initialisation code
-    fprintf(outputFile, "%s", write_init_section(boundaryList->first));
+    fprintf(outputFile, "%s", write_init_section(boundary));
 
     // Insert the actual unchanged code
     // TODO: Remove from scop the part that is not in the iteration domain (ie handled by manual code generation)
@@ -103,6 +67,55 @@ void generateCode(TCD_BoundaryList boundaryList)
 
     // Iteration code
     // TODO: Insert the iteration code here
+}
+
+void generateCode(TCD_BoundaryList boundaryList)
+{
+    osl_scop_p scop = tcdFlowData->scop;
+    osl_statement_p statement;
+
+    CloogState *state;
+    CloogInput *input;
+    CloogOptions *options;
+    struct clast_stmt *root;
+
+    state = cloog_isl_state_malloc(isl_ctx_alloc());
+    options = cloog_options_malloc(state);
+
+    FILE *outputFile = fopen(tcdFlowData->outputFile, "w+");
+
+    if (outputFile == NULL)
+    {
+        fprintf(stderr, "Error: Unable to open file %s\n", tcdFlowData->outputFile);
+        exit(EXIT_FAILURE);
+    }
+
+    // generation
+    TCD_Boundary boundary = boundaryList->first;
+    while (boundary != NULL)
+    {
+        input = cloog_input_from_osl_scop(state, scop);
+
+        if (input == NULL)
+        {
+            fprintf(stderr, "Error: Unable to generate input from scop\n");
+            exit(EXIT_FAILURE);
+        }
+
+        cloog_input_dump_cloog(stdout, input, options);
+
+        root = cloog_clast_create_from_input(input, options);
+
+        if (root == NULL)
+        {
+            fprintf(stderr, "Error: Unable to generate clast from input\n");
+            exit(EXIT_FAILURE);
+        }
+
+        generateCodeSegment(root, options, boundary, outputFile);
+        boundary = boundary->next;
+        scop = scop->next;
+    }
 
     // Finalisation code
     fprintf(outputFile, "}\n");
