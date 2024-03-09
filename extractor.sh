de731e90a095025c26deadeae30207133b4ea69b
Divino Schaeffer <divino@OrdinatdeDivino.lan>
2024-03-09 16:23:46 +0100

    compatible mac os

--- extractor.sh
+++ extractor.sh
@@ -6,8 +6,8 @@ collapse_count_file="$3"
 
 cp "$input_file" "$exit_file"
 
-sed -n 's/#pragma trahrhe collapse(\([0-9]\+\))/\1/p' "$input_file" > "$collapse_count_file"
+awk '/#pragma trahrhe collapse\([0-9]+\)/ { match($0, /[0-9]+/); print substr($0, RSTART, RLENGTH) }' "$input_file" > "$collapse_count_file"
 
-sed -i 's/#pragma trahrhe collapse([0-9]\+)/#pragma scop/g' "$exit_file"
+awk '{ gsub(/#pragma trahrhe collapse\([0-9]+\)/, "#pragma scop"); print }' "$exit_file" > tmpfile && mv tmpfile "$exit_file"
 
-sed -i 's/#pragma endtrahrhe/#pragma endscop/g' "$exit_file"
+awk '{ gsub(/#pragma endtrahrhe/, "#pragma endscop"); print }' "$exit_file" > tmpfile && mv tmpfile "$exit_file"
