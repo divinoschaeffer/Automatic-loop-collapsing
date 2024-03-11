572767b81da29dd0d8425434e18e0d8394efd52d
Nongma Sorgho <57603029+Atepir@users.noreply.github.com>
2024-03-11 11:10:03 +0100

    Update flow.c

--- src/flow.c
+++ src/flow.c
@@ -18,23 +18,34 @@ void initTcdFlow(char *inputFilename, char *outputFilename)
     tcdFlowData = (TCD_FlowData *)malloc(sizeof(TCD_FlowData));
 
     char *bash_command = (char *)malloc(100 * sizeof(char));
-    sprintf(bash_command, "extractor.sh %s", inputFilename);
+    char *pwd = (char *)malloc(100 * sizeof(char));
+    char *scoped = (char *)malloc(100 * sizeof(char));
+    char *collapse_parameters = (char *)malloc(100 * sizeof(char));
 
+    getcwd(pwd, 100);
+    strcpy(scoped, "scoped.c");
+    strcpy(collapse_parameters, "parameters.txt");
+
+    sprintf(bash_command, "%s/extractor.sh %s %s %s", pwd, inputFilename, scoped, collapse_parameters);
     system(bash_command);
 
-    tcdFlowData->entryFile = "scoped_loop.c";
+    tcdFlowData->entryFile = scoped;
     tcdFlowData->outputFile = outputFilename;
     tcdFlowData->scop = NULL;
 
-    FILE *parameters = fopen("collapse_count.txt", "r");
+    FILE *parameters = fopen(collapse_parameters, "r");
+    FILE *copy = fopen(collapse_parameters, "r");
+
     int scop_count;
-    fscanf(parameters, "%d", &scop_count);
+    fscanf(copy, "%d", &scop_count);
 
     tcdFlowData->collapseParameters = (int *)malloc(scop_count * sizeof(int));
+
     for (int i = 0; i < scop_count; i++)
     {
         fscanf(parameters, "%d", &tcdFlowData->collapseParameters[i]);
     }
+
     fclose(parameters);
     free(bash_command);
 }
