92ac5d752669eac2bb653e6604e4e233a6876adb
Divino Schaeffer <divino@OrdinatdeDivino.lan>
2024-03-11 18:41:15 +0100

    some tests

--- fusion/fusion_tests/test2/input.c
+++ fusion/fusion_tests/test2/input.c
@@ -17,3 +17,14 @@ for(i = 0; i < N - 1; i++) {
 #pragma endscop
 
 int bonjour = 0;
+
+#pragma scop
+for(i = 0; i < N - 1; i++) {
+    for(j = i + 1; j < N; j++) {
+        for(k = 0; k < N; k++) {
+            A[i][j] += B[k][i] * C[k][j];
+        }
+        A[j][i] = A[i][j];
+    }
+}
+#pragma endscop
