61446efbe835346929f7d277e9e93d69926f800b
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-03-20 22:19:40 +0100

    exec build_tests.sh and verification

--- test.sh
+++ test.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+build_script="./build_test.sh"
+
+if [ -f "$build_script" ]; then
+
+    chmod +x "$build_script"
+    
+    "$build_script"
+else
+    echo "Le fichier build_test.sh n'existe pas."
+    exit 1
+fi
