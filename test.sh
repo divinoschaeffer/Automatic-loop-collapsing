6a58d0a98a5bc940a3d7d3a22ad0e43694c2030d
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-04-11 14:15:43 +0200

    check executable presence

--- test.sh
+++ test.sh
@@ -3,18 +3,16 @@
 build_script="./build_test.sh"
 parent_directory="build/cases/"
 
+# Vérification de l'existence du script de construction
 if [ -f "$build_script" ]; then
-
-    chmod +x "build_test.sh"
-    
+    chmod +x "$build_script"
     "$build_script"
 else
     echo "Le fichier build_test.sh n'existe pas."
     exit 1
 fi
 
-cd "$tests_folder"
-
+# Vérification de l'existence et de l'exécution des exécutables dans chaque sous-dossier
 for sub_directory in "$parent_directory"/*; do
     if [ -d "$sub_directory" ]; then
         echo "Traitement du sous-dossier $(basename "$sub_directory") :"
@@ -22,16 +20,29 @@ for sub_directory in "$parent_directory"/*; do
         executable1="$sub_directory/symm"
         executable2="$sub_directory/symm_collapsed"
         
-        executable1_absolute=$(realpath "$executable1")
-        executable2_absolute=$(realpath "$executable2")
+        # Vérification de l'existence et de l'exécution de l'exécutable 1
+        if [ -x "$executable1" ]; then
+            executable1_absolute=$(realpath "$executable1")
+            exit1=$("$executable1_absolute")
+        else
+            echo "L'exécutable $executable1 est manquant ou non exécutable."
+            exit 1
+        fi
         
-        exit1=$("$executable1_absolute")
-        exit2=$("$executable2_absolute")
+        # Vérification de l'existence et de l'exécution de l'exécutable 2
+        if [ -x "$executable2" ]; then
+            executable2_absolute=$(realpath "$executable2")
+            exit2=$("$executable2_absolute")
+        else
+            echo "L'exécutable $executable2 est manquant ou non exécutable."
+            exit 1
+        fi
         
+        # Comparaison des sorties des exécutables
         if [ "$exit1" = "$exit2" ]; then
             echo -e "\e[32mTest réussi\e[0m"
         else
-            echo -e "\e[32mEchec du test\e[0m"
+            echo -e "\e[31mÉchec du test\e[0m"
         fi
         
         echo
