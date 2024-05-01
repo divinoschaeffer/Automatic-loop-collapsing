eedf310b3c8fde4817a51c782f923aedcc62c8df
SORGHO Charles <atepir0@gmail.com>
2024-05-01 16:13:38 +0200

    hotfix: fixes as per Pr. CLAUSS ' feedback on back variables reinitialization and wrong isl domains

--- src/codegen.c
+++ src/codegen.c
@@ -87,6 +87,7 @@ void write_init_section(TCD_Boundary boundary)
  * @param name_array
  * @param stop_conditions
  * @param stop_conditions_int
+ * @param initial_values
  * @param options
  * @return
  */
@@ -95,6 +96,7 @@ void increment(int curr_depth,
                char **name_array,
                struct clast_expr *stop_conditions[],
                int *stop_conditions_int,
+               struct clast_expr *initial_values[],
                CloogOptions *options)
 {
     if (curr_depth == 0)
@@ -103,14 +105,13 @@ void increment(int curr_depth,
     }
     // the innermost loop is incremented first, then when it reaches its upper bound, the next loop is incremented  etc.
     char *tmp = (char *)malloc(1024 * sizeof(char));
-    // sprintf(tmp, "\tif (%s >= %s)\n", name_array[curr_depth], take(curr_depth, outer_var_bounds));
     FILE *tmpFile = fopen("tmp2.c", "w+");
     if (tmpFile == NULL)
     {
         fprintf(stderr, "Error: Unable to open file tmp.c\n");
         exit(EXIT_FAILURE);
     }
-    clast_pprint_expr(options, tmpFile, stop_conditions[curr_depth - 1]);
+    clast_pprint_expr(options, tmpFile, stop_conditions[curr_depth]);
     fseek(tmpFile, 0, SEEK_END);
     long fsize = ftell(tmpFile);
     fseek(tmpFile, 0, SEEK_SET);
@@ -127,8 +128,6 @@ void increment(int curr_depth,
     {
         sprintf(tmp, "if (%s < %s)", name_array[curr_depth + 1], string);
     }
-    // remove tmp2.c
-    remove("tmp2.c");
 
     // print
     fs_writef("%s", tmp);
@@ -137,19 +136,33 @@ void increment(int curr_depth,
     fs_tabular();
 
     fs_writef("%s++;", name_array[curr_depth]);
-    if (stop_conditions_int[curr_depth - 1] == 1)
+    // increment the next loop - initial value
+    tmpFile = fopen("tmp3.c", "w+");
+    if (tmpFile == NULL)
     {
-        fs_writef("%s = %s - 1;", name_array[curr_depth + 1], name_array[curr_depth]);
+        fprintf(stderr, "Error: Unable to open file tmp.c\n");
+        exit(EXIT_FAILURE);
     }
-    else
-    {
-        fs_writef("%s = %s + 1;", name_array[curr_depth + 1], name_array[curr_depth]);
-    }
-    increment(curr_depth - 1, outer_var_bounds, name_array, stop_conditions, stop_conditions_int, options);
+
+    clast_pprint_expr(options, tmpFile, initial_values[curr_depth]);
+    fseek(tmpFile, 0, SEEK_END);
+    fsize = ftell(tmpFile);
+    fseek(tmpFile, 0, SEEK_SET);
+    string = (char *)malloc(fsize + 1);
+    fread(string, 1, fsize, tmpFile);
+    fclose(tmpFile);
+    string[fsize] = 0;
+    tabString(tmpFile, string, fsize);
+
+    fs_writef("%s = %s;", name_array[curr_depth + 1], string);
+
+    increment(curr_depth - 1, outer_var_bounds, name_array, stop_conditions, stop_conditions_int, initial_values, options);
 
     fs_untabular();
-
     fs_writef("}");
+
+    remove("tmp2.c");
+    remove("tmp3.c");
 }
 
 /**
@@ -158,9 +171,10 @@ void increment(int curr_depth,
  * @param boundary
  * @param stop_conditions
  * @param stop_conditions_int
+ * @param initial_values
  * @param options
  */
-void write_increment_section(TCD_Boundary boundary, struct clast_expr *stop_conditions[], int *stop_conditions_int, CloogOptions *options)
+void write_increment_section(TCD_Boundary boundary, struct clast_expr *stop_conditions[], int *stop_conditions_int, struct clast_expr *initial_values[], CloogOptions *options)
 {
     char *outer_var_bounds = boundary->outerLoopUpperBound;
     char **name_array = boundary->nameArray;
@@ -168,7 +182,7 @@ void write_increment_section(TCD_Boundary boundary, struct clast_expr *stop_cond
     int max_depth = tcdFlowData->collapseParameters[boundary_index];
     fs_tabular();
     fs_writef("%s++;", name_array[max_depth]);
-    increment(max_depth - 1, outer_var_bounds, name_array, stop_conditions, stop_conditions_int, options);
+    increment(max_depth - 1, outer_var_bounds, name_array, stop_conditions, stop_conditions_int, initial_values, options);
     fs_untabular();
 }
 
@@ -193,7 +207,9 @@ void generateCodeSegment(struct clast_stmt *root, CloogOptions *options, TCD_Bou
     // DONE - Remove from scop the part that is not in the iteration domain (ie handled by manual code generation)
     // while we are have not hit the depth of parallelism of the boundary, we need to go deeper in the loop nest
     int loop_nest_depth = tcdFlowData->collapseParameters[boundary_index];
+    int max_depth = loop_nest_depth;
     struct clast_expr *stop_conditions[loop_nest_depth];
+    struct clast_expr *initial_values[loop_nest_depth];
     int *stop_conditions_int = (int *)malloc(loop_nest_depth * sizeof(int));
     while (loop_nest_depth > 0)
     {
@@ -207,16 +223,17 @@ void generateCodeSegment(struct clast_stmt *root, CloogOptions *options, TCD_Bou
         else if (CLAST_STMT_IS_A(root, stmt_for))
         {
             struct clast_for *for_stmt = (struct clast_for *)root;
+            initial_values[max_depth - loop_nest_depth] = for_stmt->LB;
             root = for_stmt->body;
             if (for_stmt->UB != NULL)
             {
-                stop_conditions[loop_nest_depth - 1] = for_stmt->UB;
-                stop_conditions_int[loop_nest_depth - 1] = 0;
+                stop_conditions[max_depth - loop_nest_depth] = for_stmt->UB;
+                stop_conditions_int[max_depth - loop_nest_depth] = 0;
             }
             else
             {
-                stop_conditions[loop_nest_depth - 1] = for_stmt->LB;
-                stop_conditions_int[loop_nest_depth - 1] = 1;
+                stop_conditions[max_depth - loop_nest_depth] = for_stmt->LB;
+                stop_conditions_int[max_depth - loop_nest_depth] = 1;
             }
             loop_nest_depth--;
         }
@@ -264,7 +281,7 @@ void generateCodeSegment(struct clast_stmt *root, CloogOptions *options, TCD_Bou
     fs_writef("%s", tabStringReturn(string, fsize));
 
     // Increment
-    write_increment_section(boundary, stop_conditions, stop_conditions_int, options);
+    write_increment_section(boundary, stop_conditions, stop_conditions_int, initial_values, options);
 
     // Finalisation code
     fs_writef("}\n");
