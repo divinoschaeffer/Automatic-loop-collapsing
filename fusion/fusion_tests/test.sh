0baec1f3ec9026d3e495017e8a0466543010c321
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-05-15 23:37:59 +0200

    functionnal test

---
+++ fusion/fusion_tests/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Fonction pour vérifier les fichiers
+check_files() {
+    local test_name="$1"
+
+    # Vérification de output.c
+    if diff "${test_name}/output.c" "${test_name}/verif.c" &> /dev/null; then
+        echo "$(tput setaf 2)Test réussi$(tput sgr0): Le fichier output.c pour $test_name est identique à verif.c."
+    else
+        echo "$(tput setaf 1)Test échoué$(tput sgr0): Le fichier output.c pour $test_name est différent de verif.c."
+    fi
+}
+
+# Liste des tests
+tests=("test1" "test2")
+
+# Vérification pour chaque test
+for test in "${tests[@]}"; do
+    echo "Vérification des fichiers pour le test $test"
+    check_files "$test"
+done
---
+++ fusion/fusion_tests/test2/verif.c
@@ -0,0 +1,63 @@
+#include 'new.h'
+#include <stdio.h>
+
+#define N 1000
+
+int i, j, k;
+double A[N][N], B[N][N], C[N][N];
+
+unsigned pc_0;
+unsigned upper_bound_0 = Ehrhart0(N);
+unsigned first_iteration_0 = 1;
+#pragma omp parallel for private(i, j, k) firstprivate(first_iteration_0) schedule(static)
+for (pc_0 = 1; pc_0 <= upper_bound_0; pc_0++)
+{
+    if (first_iteration_0)
+    {
+        i = trahrhe_i0(pc_0, N);
+        j = trahrhe_j0(pc_0, i, N);
+        first_iteration_0 = 0;
+    }
+
+    for (k = 0; k <= N - 1; k++)
+    {
+        A[i][j] += B[k][i] * C[k][j];
+    }
+    A[j][i] = A[i][j];
+
+    j++;
+    if (j < N - 1)
+    {
+        i++;
+        j = i + 1;
+    }
+}
+
+int bonjour = 0;
+
+unsigned pc_0;
+unsigned upper_bound_0 = Ehrhart0(N);
+unsigned first_iteration_0 = 1;
+#pragma omp parallel for private(i, j, k) firstprivate(first_iteration_0) schedule(static)
+for (pc_0 = 1; pc_0 <= upper_bound_0; pc_0++)
+{
+    if (first_iteration_0)
+    {
+        i = trahrhe_i0(pc_0, N);
+        j = trahrhe_j0(pc_0, i, N);
+        first_iteration_0 = 0;
+    }
+
+    for (k = 0; k <= N - 1; k++)
+    {
+        A[i][j] += B[k][i] * C[k][j];
+    }
+    A[j][i] = A[i][j];
+
+    j++;
+    if (j < N - 1)
+    {
+        i++;
+        j = i + 1;
+    }
+}
